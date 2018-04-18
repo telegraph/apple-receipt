d4fcabc4eb81aecff0c73a0520df7be1c66b27f7
Crenguta Soldatu <crenguta.soldatu@3pillarglobal.com>
2018-04-18 16:25:59 +0300

    @JsonIgnore

--- src/main/scala/uk/co/telegraph/applereceipt/Receipt.scala
+++ src/main/scala/uk/co/telegraph/applereceipt/Receipt.scala
@@ -1,11 +1,11 @@
 package uk.co.telegraph.applereceipt
 
-import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.{JsonCreator, JsonIgnore, JsonIgnoreProperties}
 
 object Receipt {
   def receipt(receiptData: String) = new Receipt(receiptData)
 }
 
-class Receipt @JsonCreator private(val receiptData: String) {
+class Receipt @JsonCreator @JsonIgnoreProperties private(val receiptData: String) {
   def getReceiptData: String = receiptData
 }
