7fea41f808221bf542cf385a5fc474a433ef64dd
marinela.neagu <marinela.neagu@3pillarglobal.com>
2018-04-18 16:54:22 +0300

    add @JsonCreator

--- src/main/scala/uk/co/telegraph/applereceipt/Receipt.scala
+++ src/main/scala/uk/co/telegraph/applereceipt/Receipt.scala
@@ -6,6 +6,6 @@ object Receipt {
   def receipt(receiptData: String) = new Receipt(receiptData)
 }
 
-class Receipt @JsonIgnoreProperties private(val receiptData: String) {
+class Receipt @JsonCreator private(val receiptData: String) {
   def getReceiptData: String = receiptData
 }
